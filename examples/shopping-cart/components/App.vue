007a093a5813be95b8c13e1a7b126c18503fd57b
MangliZeng <1195473786@qq.com>
2018-09-25 16:32:05 +0800

    首次提交代码

---
+++ examples/shopping-cart/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Shopping Cart Example</h1>
+    <hr>
+    <h2>Products</h2>
+    <ProductList/>
+    <hr>
+    <ShoppingCart/>
+  </div>
+</template>
+
+<script>
+import ProductList from './ProductList.vue'
+import ShoppingCart from './ShoppingCart.vue'
+import Zml from './Zml.vue'
+
+export default {
+  components: { ProductList, ShoppingCart, Zml }
+}
+</script>
---
+++ examples/shopping-cart/components/Zml.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 id="zml">zml</h1>
+</template>
+<script>
+
+    export default {
+        name:"zml"
+    }
+</script>
